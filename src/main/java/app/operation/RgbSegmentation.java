de2a7b39e69bed5c8f0e9041f21856af289848c5
Jakub Żmuda <goodguykuba@gmail.com>
2020-02-07 23:26:11 +0100

    simple thresholding

--- src/main/java/app/operation/RgbSegmentation.java
+++ src/main/java/app/operation/RgbSegmentation.java
@@ -1,4 +1,35 @@
 package app.operation;
 
+import app.representation.Canals;
+import app.representation.ImageMap;
+import app.util.ImageConverter;
+import javafx.scene.image.Image;
+
 public class RgbSegmentation {
+    private Image inputImage;
+    private ImageMap map;
+
+    public RgbSegmentation(Image image, Canals from, Canals to) {
+        this.inputImage = image;
+
+        ImageMap imageMap = new ImageConverter().toImageMap(image);
+        imageMap.singlePointOperation((x, y, canals) -> {
+            if (fitsInRange(canals, from, to)) {
+                return new Canals(255, 255, 255);
+            }
+            return new Canals(0, 0, 0);
+        });
+
+        this.map = imageMap;
+    }
+
+    private boolean fitsInRange(Canals canals, Canals from, Canals to) {
+        return canals.red() >= from.red() && canals.red() <= to.red() &&
+                canals.blue() >= from.blue() && canals.blue() <= to.blue() &&
+                canals.green() >= from.green() && canals.green() <= to.green();
+    }
+
+    public Image map() {
+        return new ImageConverter().toImage(map);
+    }
 }
--- src/main/java/app/window/RgbSegmentationWindow.java
+++ src/main/java/app/window/RgbSegmentationWindow.java
@@ -1,17 +1,21 @@
 package app.window;
 
+import app.operation.RgbSegmentation;
 import app.representation.BinaryImage;
+import app.representation.Canals;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import sun.util.resources.cldr.st.CurrencyNames_st_LS;
 
 public class RgbSegmentationWindow {
 
@@ -20,6 +24,16 @@ public class RgbSegmentationWindow {
 
     private Image mapImage;
     private ImageView mapImageView;
+
+    private TextField redMinField;
+    private TextField redMaxField;
+
+    private TextField greenMinField;
+    private TextField greenMaxField;
+
+    private TextField blueMinField;
+    private TextField blueMaxField;
+
 
     public RgbSegmentationWindow(Image inputImage) {
         this.inputImage = inputImage;
@@ -41,6 +55,12 @@ public class RgbSegmentationWindow {
         stage.show();
     }
 
+    public void updateMap(Image newImage) {
+        mapImage = newImage;
+        mapImageView.setImage(mapImage);
+    }
+
+
     private HBox buildPreviewPanel() {
         inputImageView = new ImageView(inputImage);
         mapImage = BinaryImage.black(Math.round(inputImage.getWidth()), Math.round(inputImage.getHeight())).asImage();
@@ -61,28 +81,30 @@ public class RgbSegmentationWindow {
         container.add(new Label("max"), 2, 0);
 
         Label redLabel = new Label("czerwony");
-        TextField redMinField = new TextField("0");
-        TextField redMaxField = new TextField("255");
+        redMinField = new TextField("0");
+        redMaxField = new TextField("255");
         container.add(redLabel, 0, 1);
         container.add(redMinField, 1, 1);
         container.add(redMaxField, 2, 1);
 
         Label greenLabel = new Label("zielony");
-        TextField greenMinField = new TextField("0");
-        TextField greenMaxField = new TextField("255");
+        greenMinField = new TextField("0");
+        greenMaxField = new TextField("255");
         container.add(greenLabel, 0, 2);
         container.add(greenMinField, 1, 2);
         container.add(greenMaxField, 2, 2);
 
         Label blueLabel = new Label("niebieski");
-        TextField blueMinField = new TextField("0");
-        TextField blueMaxField = new TextField("255");
+        blueMinField = new TextField("0");
+        blueMaxField = new TextField("255");
         container.add(blueLabel, 0, 3);
         container.add(blueMinField, 1, 3);
         container.add(blueMaxField, 2, 3);
 
         Button doIt = new Button("Podgląd");
         doIt.setOnAction((event) -> {
+            Image map = new RgbSegmentation(inputImage, thresholdFrom(), thresholdTo()).map();
+            updateMap(map);
 //            Image newImage = new ImageOperations().threshold(image, Integer.parseInt(redMinField.getText()), Integer.parseInt(minField.getText()), Integer.parseInt(maxField.getText()));
 //            updateImage(newImage);
 //            stage.close();
@@ -94,5 +116,13 @@ public class RgbSegmentationWindow {
         container.add(doIt, 1, 4);
 
         return container;
+    }
+
+    private Canals thresholdFrom() {
+        return new Canals(Integer.parseInt(redMinField.getText()), Integer.parseInt(greenMinField.getText()), Integer.parseInt(blueMinField.getText()));
+    }
+
+    private Canals thresholdTo() {
+        return new Canals(Integer.parseInt(redMaxField.getText()), Integer.parseInt(greenMaxField.getText()), Integer.parseInt(blueMaxField.getText()));
     }
 }
